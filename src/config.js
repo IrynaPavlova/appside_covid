7128ee03e9cca029407cde2c1ad385c179e5994b
Pavlova <irochkapavlova85@gmail.com>
2020-07-28 02:43:44 +0300

    some fix

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 import "dotenv/config.js";
-console.log("process.env", process.env);
+//console.log("process.env", process.env);
 
 export default {
-  googleApiKey:
-    //"AIzaSyBxgswoPm6raa1oW6pkW4P2A_V0IrZ4ApQ"
-    process.env.GOOGLE_API_KEY
+  googleApiKey: "AIzaSyBxgswoPm6raa1oW6pkW4P2A_V0IrZ4ApQ"
+  //process.env.GOOGLE_API_KEY
 };
