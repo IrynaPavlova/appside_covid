5c32593ffd5c4d4d3abc33848c1e83b02d534bb1
Pavlova <irochkapavlova85@gmail.com>
2020-07-28 22:40:21 +0300

    edits

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("process.env", process.env);
+//console.log("process.env", process.env);
 
 export default {
   googleApiKey: "AIzaSyBxgswoPm6raa1oW6pkW4P2A_V0IrZ4ApQ"
