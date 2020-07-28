26279b9347a699a168d1b8952d85f8c995853483
Pavlova <irochkapavlova85@gmail.com>
2020-07-28 22:37:26 +0300

    edits

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 console.log("process.env", process.env);
 
 export default {
-  googleApiKey:
-    //"AIzaSyBxgswoPm6raa1oW6pkW4P2A_V0IrZ4ApQ"
-    process.env.REACT_APP_GOOGLE_API_KEY
+  googleApiKey: "AIzaSyBxgswoPm6raa1oW6pkW4P2A_V0IrZ4ApQ"
+  //process.env.REACT_APP_GOOGLE_API_KEY
 };
